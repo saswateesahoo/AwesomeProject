d6fbb3f3e3ef67d32fd621a3ef330148801e9eec
saswati <saswatisahoo2k@gmail.com>
2023-10-18 11:47:31 +0530

    locationpage inprogress

---
+++ Screens/HomeScreen.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome to Map App</Text>
+      <Button title="Open Map" onPress={() => navigation.navigate('Map')} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: 'green',
+  },
+});
+
+export default HomeScreen;
